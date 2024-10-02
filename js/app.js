e7e9a80ee425481a2b5634ca6652e7c7034531e1
Benjamin Ting <tingzhb@gmail.com>
2024-10-02 14:21:45 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Declare or initialize a variable.
+let gold = 10;
+
+console.log(gold); // 10
+
+// Increase the variable by 2.
+gold += 2;
+console.log(gold); // 12
+
+// Multiply the variable by 2.
+gold *= 2;
+console.log(gold); // 24
+
+// Increase the variable by 1.
+gold++;
+console.log(gold); // 25
+
+// Decrease the variable by 1.
+gold--;
+console.log(gold);
+
+
+// Implement a program that takes an input for km/h and displays the result in m/s.
+// Input: 72
+// Output: 20
+
+let speedInput = 72;
+speedInput /= 3.6;
+console.log(speedInput);
+
+// Implement a program that takes a number input for minutes and displays the result in seconds.
+// Input: 3
+// Output: 180
+
+let minutesInput = 3;
+
+
+console.log(minutesInput);
